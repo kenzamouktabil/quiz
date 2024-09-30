6acbc2aa39e1fe038b13f7bad70ef6646ed72be8
kenzamouktabil <kenzamouktabil@gmail.com>
2024-09-30 12:17:41 +0100

    Initial commit

---
+++ questions.js
@@ -0,0 +1,65 @@
+let questions =[
+    {
+        numb: 1 ,
+        question: "What’s the first step when your website breaks?",
+        answer: "C. Refresh the page , pray and hope it fixes itself",
+        options:[
+            "A. Revert all your recent changes ",
+            "B. Blame JavaScript",
+            "C. Refresh the page , pray and hope it fixes itself",
+            "D. Blame the last person who touched the code"
+
+        ]
+    },
+    {
+        numb: 2 ,
+        question: "What does CSS stand for",
+        answer: "A. Cascading Style Sheet",
+        options:[
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Cool Style Sheet",
+            "D. Cyber Style Surfing"
+
+        ]
+    },
+    {
+        numb: 3 ,
+        question: "What is the function of “cookies” in a web browser?",
+        answer: "A. To store small pieces of data about websites you visit",
+        options:[
+            "A. To store small pieces of data about websites you visit",
+            "B. To make websites taste better",
+            "C. To deliver fresh-baked goods to your inbox",
+            "D. To give you a sweet online experience"
+
+        ]
+    },
+    {
+        numb: 4  ,
+        question: "What does 'API' stand for",
+        answer: "D. Application Programming Interface",
+        options:[
+            "A. Awesome Programming Interface",
+            "B. Automated Penguin Integration",
+            "C. Another Pizza Incoming",
+            "D. Application Programming Interface"
+        ]
+    },
+    {
+        numb: 5 ,
+        question: "Why do developers love dark mode?",
+        answer: "B. It makes us feel like hackers from the movies",
+        options:[
+            "A. Because coding in daylight is too mainstream",
+            "B. It makes us feel like hackers from the movies",
+            "C. It’s better for spying on the matrix",
+            "D. Light mode hurts our delicate eyes"
+        ]
+    },
+];
+
+
+
+
+
